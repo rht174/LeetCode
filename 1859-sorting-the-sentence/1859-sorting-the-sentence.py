fc3be7a73d8a0c8cf3e865de0182f0165f1fe7ea
Rohit Kushwah <65383044+rht174@users.noreply.github.com>
2022-08-24 03:09:13 +0530

    Update 1859-sorting-the-sentence.py

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -3,9 +3,6 @@ class Solution:
         ls = s.split()
         new = [None] * len(ls)
         for word in ls:
-        
             index = int(word[-1]) - 1
-         
-            new[index] = word[:-1]
-            
+            new[index] = word[:-1]   
         return " ".join(i for i in new)
